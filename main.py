f902a4088afa51bd5787c5c70831ebb21376798c
Hussam Alafandi <hosam.alafandi@gmail.com>
2026-02-07 23:06:05 +0100

    move data loading and cleaning from main to analyzer

--- main.py
+++ main.py
@@ -1,36 +1,7 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-
-
-def load_data(file_path):
-    try:
-        df = pd.read_csv(file_path)
-    except FileNotFoundError:
-        print(f"Error: File '{file_path}' not found.")
-        raise
-    return df
-
-def clean_data(df):
-    # Fill missing values with mean for numerical columns
-    numerical_cols = df.select_dtypes(include=[np.number]).columns
-    for col in numerical_cols:
-        df[col] = df[col].fillna(df[col].mean())
-
-    # Delete rows with missing values in string columns
-    string_cols = df.select_dtypes(include=[str]).columns
-    df.dropna(subset=string_cols, inplace=True)
-
-    # Convert date column to datetime
-    df['order_date'] = pd.to_datetime(df['order_date'], errors='coerce')
-
-    # Remove duplicates
-    df.drop_duplicates(inplace=True)
-
-    return df
+from analyzer import load_data, clean_data
 
 try:
-    df = load_data('./data/sales_dataa.csv')
+    df = load_data('./data/sales_data.csv')
 except FileNotFoundError:
     exit(1)
 
