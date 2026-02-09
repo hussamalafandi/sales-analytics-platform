b0b48c8acb482ad6b3ee5234e2f4deef7844212d
Hussam Alafandi <hosam.alafandi@gmail.com>
2026-02-09 11:21:18 +0100

    Add visualization functions to create and save various charts for analytics

---
+++ utils.py
@@ -0,0 +1,8 @@
+import os
+
+def create_output_directory():
+    """Create output directory if it doesn't exist"""
+    output_dir = "output"
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    
